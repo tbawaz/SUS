47feb93fc247dfc32de7a28f6a7653a523cfeba0
steven <smelena@uci.edu>
2015-02-24 16:35:24 -0800

    sus page update

--- suspage/sus.js
+++ suspage/sus.js
@@ -1,72 +1,53 @@
 window.onload = function () {
+		var chart = new CanvasJS.Chart("chartContainer",
+    {
+      backgroundColor:  "#F5F5F5",
+      title:{
+      text: "Units Consumed($)",
+      },
+       data: [
+      {
+        type: "line",
+
+        dataPoints: [
+        { x: new Date(2014, 00, 1), y: 50 },
+        { x: new Date(2014, 01, 1), y: 55 },
+        { x: new Date(2014, 02, 1), y: 62 },
+        { x: new Date(2014, 03, 1), y: 46 },
+        { x: new Date(2014, 04, 1), y: 45 },
+        { x: new Date(2014, 05, 1), y: 50 },
+        { x: new Date(2014, 06, 1), y: 48 },
+        { x: new Date(2014, 07, 1), y: 48 },
+        { x: new Date(2014, 08, 1), y: 41 },
+        { x: new Date(2014, 09, 1), y: 50 },
+        { x: new Date(2014, 10, 1), y: 48 },
+        { x: new Date(2014, 11, 1), y: 51 }
+        ]
+      },
+     {
+	type: "line",
+	color: "red",
+        dataPoints: [
+        { x: new Date(2014, 00, 1), y: 60 },
+        { x: new Date(2014, 01, 1), y: 65 },
+        { x: new Date(2014, 02, 1), y: 72 },
+        { x: new Date(2014, 03, 1), y: 56 },
+        { x: new Date(2014, 04, 1), y: 55 },
+        { x: new Date(2014, 05, 1), y: 60 },
+        { x: new Date(2014, 06, 1), y: 58 },
+        { x: new Date(2014, 07, 1), y: 58 },
+        { x: new Date(2014, 08, 1), y: 51 },
+        { x: new Date(2014, 09, 1), y: 60 },
+        { x: new Date(2014, 10, 1), y: 58 },
+        { x: new Date(2014, 11, 1), y: 61 }
+        ]
+     }
+      ]
+    });
+
+    chart.render();
+  }
 
 	
-//Better to construct options first and then pass it as a parameter
-	var line = {
-		backgroundColor: "#F5F5F5",
-		color:"#F5F5F5",
-		title: {
-        text: "SUS Data",  
-        fontColor: "#3385AD",
-        fontSize: 30,
-        padding: 10,
-        margin: 15,
-		interactivityEnabled: true,
-        fontWeight: "bold"
-      },
-                animationEnabled: true,
-		data: [
-		{
-			markerColor: "#94D60A",
-			color: "#94D60A",
-			type: "line", //change it to line, area, bar, pie, etc
-			dataPoints: [
-				{ x: 10, y: 10 },
-				{ x: 20, y: 11 },
-				{ x: 30, y: 14 },
-				{ x: 40, y: 16 },
-				{ x: 50, y: 19 },
-				{ x: 60, y: 15 },
-				{ x: 70, y: 12 },
-				{ x: 80, y: 10 }
-			]
-		}
-		]
-	};
-	
-	var column = {
-		backgroundColor: "#F5F5F5",
-		color:"#F5F5F5",
-		title: {
-        text: "SUS Data",  
-        fontColor: "#3385AD",
-        fontSize: 30,
-        padding: 10,
-        margin: 15,
-		interactivityEnabled: true,
-        fontWeight: "bold"
-      },
-                animationEnabled: true,
-		data: [
-		{
-			markerColor: "#94D60A",
-			color: "#94D60A",
-			type: "column", //change it to line, area, bar, pie, etc
-			dataPoints: [
-				{ x: 10, y: 10 },
-				{ x: 20, y: 11 },
-				{ x: 30, y: 14 },
-				{ x: 40, y: 16 },
-				{ x: 50, y: 19 },
-				{ x: 60, y: 15 },
-				{ x: 70, y: 12 },
-				{ x: 80, y: 10 }
-			]
-		}
-		]
-	};
 
-	$("#chartLine").CanvasJSChart(line);
-	$("#chartColumn").CanvasJSChart(column);
 
-}
